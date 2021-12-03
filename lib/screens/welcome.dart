91e8e211343239aa60e3f988201258926e9f3bcc
m0wer <m0wer@autistici.org>
2021-12-03 13:03:25 +0300

    feat: add description and keywords

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -9,17 +9,28 @@ class WelcomeScreenBody extends StatelessWidget {
     return Container(
         padding: const EdgeInsets.all(40.0),
         child: ListView(children: [
-          Text("Welcome to now8!",
+          SelectableText("Welcome to now8!",
               style: Theme.of(context).textTheme.headline4!,
               textAlign: TextAlign.center),
           Container(
               margin: const EdgeInsets.only(top: 40),
-              child: Text(
+              child: SelectableText(
                 "now8 is a public transport app that provides "
                 "improved vehicle arrival time estimations "
                 "using Machine Learning.",
                 style: Theme.of(context).textTheme.headline6!,
-              ))
+              )),
+          Card(
+              margin: const EdgeInsets.all(40.0),
+              child: Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: SelectableText(
+                    "Features: \n"
+                    "\n"
+                    "    Support for Madrid (Spain).\n"
+                    "    Fuzzy search for stop names and codes.\n",
+                    style: Theme.of(context).textTheme.headline6!,
+                  ))),
         ]));
   }
 }
