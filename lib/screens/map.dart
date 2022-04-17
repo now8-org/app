8ed14e49512359fb5798d7a83d800a664406d64e
m0wer <m0wer@autistici.org>
2022-04-17 16:44:01 +0300

    feat(screens/map): add stops with marker clusters

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:now8/providers.dart';
 import 'package:now8/screens/common.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 
 class MapScreen extends StatelessWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -21,10 +24,32 @@ class MapScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<dynamic>? stops = Provider.of<StopsProvider>(context).stops;
+    if (stops == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    var markers = stops
+        .map((var stop) => Marker(
+              point: LatLng(stop["latitude"], stop["longitude"]),
+              builder: (ctx) => Icon(Icons.pin_drop),
+            ))
+        .toList();
+    //var markers = [
+    //  Marker(
+    //    anchorPos: AnchorPos.align(AnchorAlign.center),
+    //    height: 30,
+    //    width: 30,
+    //    point: LatLng(53.3498, -6.2603),
+    //    builder: (ctx) => Icon(Icons.pin_drop),
+    //  ),
+    //];
     return FlutterMap(
       options: MapOptions(
         center: LatLng(40.4165, -3.70256),
         zoom: 13.0,
+        plugins: [
+          MarkerClusterPlugin(),
+        ],
       ),
       layers: [
         TileLayerOptions(
@@ -45,6 +70,24 @@ class MapScreenBody extends StatelessWidget {
               ),
             ),
           ],
+        ),
+        MarkerClusterLayerOptions(
+          maxClusterRadius: 120,
+          size: Size(40, 40),
+          fitBoundsOptions: FitBoundsOptions(
+            padding: EdgeInsets.all(50),
+          ),
+          markers: markers,
+          polygonOptions: PolygonOptions(
+              borderColor: Colors.blueAccent,
+              color: Colors.black12,
+              borderStrokeWidth: 3),
+          builder: (context, markers) {
+            return FloatingActionButton(
+              child: Text(markers.length.toString()),
+              onPressed: null,
+            );
+          },
         ),
       ],
     );
